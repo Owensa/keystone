eb1e1283925d3acc8a14d8e5b6f091db9501ea7d
Andrew Owens <drew.owensg@gmail.com>
2016-11-30 08:39:39 -0500

    adding data models, media, timeline, music, users, contacts, and their related models

--- keystone.js
+++ keystone.js
@@ -1,11 +1,55 @@
-var express = require("express");
-var app = express();
+var keystone = require('keystone');
 
-app.get('/', function(req, res) {
-  res.send('alright sweet its working');
+keystone.init({
+
+	'name': 'Victoria Justice',
+	'brand': 'Victoria Justice',
+
+	'favicon': 'public/favicon.ico',
+	'less': 'public',
+	'static': 'public',
+
+	'views': 'templates/views',
+	'view engine': 'jade',
+
+	'auto update': true,
+	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://heroku_z28h7plc:i3tr2ih91ekgn9cu6042k6inun@ds113628.mlab.com:13628/heroku_z28h7plc',
+	'cloudinary config': 'cloudinary://427439183981365:6nhiIF1IPezQbDx9_LQoHIjnEPc@hyt7folyc',
+
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': process.env.COOKIE_SECRET || '9c977427f76b88ba2f2b72fdd5d5e3f082beaf66e58709083d04bd84f670110a',
+
+	'ga property': process.env.GA_PROPERTY,
+	'ga domain': process.env.GA_DOMAIN,
+
+	'chartbeat property': process.env.CHARTBEAT_PROPERTY,
+	'chartbeat domain': process.env.CHARTBEAT_DOMAIN
+
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+	ga_property: keystone.get('ga property'),
+	ga_domain: keystone.get('ga domain'),
+	chartbeat_property: keystone.get('chartbeat property'),
+	chartbeat_domain: keystone.get('chartbeat domain')
 });
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	'posts': ['posts', 'post-comments', 'post-categories'],
+	'galleries': 'galleries',
+	'enquiries': 'enquiries',
+	'users': 'users',
+	'field-tests': 'things'
+});
+
+keystone.start();
