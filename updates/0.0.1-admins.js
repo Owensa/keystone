3e110a2868ac71419f539fd0f5ede09199eb7f99
Andrew Owens <drew.owensg@gmail.com>
2016-11-30 08:52:12 -0500

    reorganize proj for uploadable content. added base routes. added admin login credentials.

---
+++ updates/0.0.1-admins.js
@@ -0,0 +1,16 @@
+var keystone = require('keystone');
+var User = keystone.list('User');
+
+module.exports = function (done) {
+	new User.model({
+		name: {
+			first: 'admin',
+			last: 'admin'
+		},
+		email: 'admin@wethem.us',
+		password: 'wethemus',
+		isAdmin: true,
+		isProtected: true,
+	})
+	.save(done);
+};
